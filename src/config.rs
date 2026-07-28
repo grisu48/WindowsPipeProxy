14ad9950cc507a083f9980d661c07a3d38a878b7
phoenix <felix.niederwanger@suse.com>
2026-07-28 13:41:34 +0200

    Add program parameters
    Adds the first program parameters to the tool to allow custom
    configuration.

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use serde::Deserialize;
-use std::fs;
+use std::{env, fs};
 
 #[derive(Deserialize, Clone)]
 pub struct Config {
@@ -35,12 +35,25 @@ pub struct Plumber {
     pub reconnect_delay: u64,
 }
 
+// Handler for program arguments
+pub struct Args {
+    pub config_file: String, // Configuration file
+    args: Vec<String>,       // All collected arguments
+}
+
 impl Config {
     // Parse program arguments
     pub fn parse_file(filename: &str) -> Result<Config> {
         let contents = fs::read_to_string(filename)?;
         let config: Config = toml::from_str(contents.as_str())?;
         Ok(config)
+    }
+
+    // Apply the given program arguments
+    pub fn apply_args(&mut self, args: &Args) {
+        if args.has_flag("-v") {
+            self.verbose = true;
+        }
     }
 }
 
@@ -53,10 +66,73 @@ impl Default for Plumber {
     }
 }
 
+impl Args {
+    // Create a new Args instance from the given program arguments
+    pub fn create() -> Result<Args> {
+        let mut args = Args {
+            config_file: "pipe-proxy.toml".to_string(),
+            args: collect_args(),
+        };
+        args.parse()?;
+        return Ok(args);
+    }
+
+    // Create a new empty Args instance (mostly for testing)
+    fn new() -> Args {
+        Args {
+            config_file: "".to_string(),
+            args: Vec::new(),
+        }
+    }
+
+    // Checks if the given flag is present. flag must be passed with the leading dashes, e.g. `-v`
+    pub fn has_flag(&self, flag: &str) -> bool {
+        self.args.contains(&flag.to_string())
+    }
+
+    pub fn parse(&mut self) -> Result<()> {
+        for arg in self.args.iter() {
+            if arg.starts_with("-") {
+                // flag
+            } else {
+                self.config_file = arg.clone();
+            }
+        }
+        Ok(())
+    }
+}
+
+fn collect_args() -> Vec<String> {
+    let mut args = env::args_os();
+    args.next().expect("program name expected");
+    args.map(|x| x.into_string().expect("parameter string conversion failed"))
+        .collect()
+}
+
 fn default_reconnect_attempts() -> i32 {
     10
 }
 
 fn default_reconnect_delay() -> u64 {
     1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn args() {
+        let mut args = Args::new();
+        // Test if the -v flag gets passed and if the custom configuration file is set.
+        let flag_v = "-v".to_string();
+        let config_file = "config.toml".to_string();
+        assert!(!args.has_flag(&flag_v));
+        assert!(args.config_file.is_empty());
+        args.args = vec![flag_v.clone(), config_file.clone()];
+        args.parse().expect("args.parse failed");
+        assert!(args.has_flag(&flag_v));
+        assert!(!args.config_file.is_empty());
+        assert_eq!(args.config_file, config_file);
+    }
 }
